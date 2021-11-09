06c760098fb3a96d809f56ced5b25ad37c4fbe43
Maria Isabel Lopez <mrlpz1515@gmail.com>
2021-11-08 22:07:06 -0500

    Initial commit

---
+++ svelte-app/src/config.ts
@@ -0,0 +1,10 @@
+const API: string = process.env.SVELTE_APP_API || 'api';
+const DEV: boolean = process.env.DEV || true;
+
+function logRouteLocation(location: Object) {
+  if (process.env.ROLLUP_WATCH) {
+    console.table(location);
+  }
+}
+
+export { API, DEV, logRouteLocation };
